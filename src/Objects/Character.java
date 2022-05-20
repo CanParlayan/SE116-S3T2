280b1e9691f65c71c71766846ee17e9e81792189
Chainedge <chainedge@outlook.com>
2022-05-20 11:59:49 +0300

    Added first step Character implementations
    Signed-off-by: Chainedge <chainedge@outlook.com>

---
+++ src/Objects/Character.java
@@ -0,0 +1,92 @@
+package Objects;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+//This class will probably be abstracted from a top class that also abstracts the enemy class
+public class Character {
+    //Small initialization note, decided to handle random generation inside this class and not expose it
+    private String charClass;
+    private int strength;
+    SecureRandom rng = new SecureRandom();
+
+    public Weapon getHeldWeapon() {
+        return heldWeapon;
+    }
+
+    public void setHeldWeapon(Weapon heldWeapon) {
+        this.heldWeapon = heldWeapon;
+    }
+
+    public Armor getHeldArmor() {
+        return heldArmor;
+    }
+
+    public void setHeldArmor(Armor heldArmor) {
+        this.heldArmor = heldArmor;
+    }
+    private List<Item> inventory = new ArrayList<Item>(); //no limitation because the limit is related to weight
+    //to implement: dummy attack method, pick, wield, wear, etc...
+    private Weapon heldWeapon;
+    private Armor heldArmor;
+    public Character(String charClass) throws InvalidCharClassException {
+        this.charClass = charClass;
+        switch (charClass) {
+            case "Tank" -> {
+                this.strength = rng.nextInt(1, 6);
+                this.vitality = rng.nextInt(6, 11);
+                this.intelligence = rng.nextInt(1, 6);
+                System.out.println("Tank created with STR: " + this.strength + " VIT: " + this.vitality + " INT: " + this.intelligence);
+            }
+            case "Fighter" -> {
+                this.strength = rng.nextInt(6,11);
+                this.vitality = rng.nextInt(3,8);
+                this.intelligence = rng.nextInt(1,6);
+                System.out.println("Fighter created with STR: " + this.strength + " VIT: " + this.vitality + " INT: " + this.intelligence);
+            }
+            case "Healer" -> {
+                this.strength = rng.nextInt(3,8);
+                this.vitality = rng.nextInt(1,6);
+                this.intelligence = rng.nextInt(6,11);
+                System.out.println("Healer created with STR: " + this.strength + " VIT: " + this.vitality + " INT: " + this.intelligence);
+            }
+            default ->
+                    throw new InvalidCharClassException(this.charClass); //very intelligently designed exception designed to catch invalid class names
+        }
+    }
+
+    private int intelligence;
+    private int vitality;
+
+    public String getCharClass() {
+        return charClass;
+    }
+
+    public void setCharClass(String charClass) {
+        this.charClass = charClass;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getVitality() {
+        return vitality;
+    }
+
+    public void setVitality(int vitality) {
+        this.vitality = vitality;
+    }
+}
