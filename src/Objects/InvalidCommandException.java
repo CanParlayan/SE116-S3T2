510fcf542fda7fe301792d89aa8c1511f1f55493
CanParlayan <can.parlayan@std.ieu.edu.tr>
2022-06-11 22:14:51 +0300

    deleted

--- src/Objects/InvalidCommandException.java
+++
@@ -1,7 +0,0 @@
-package Objects;
-
-public class InvalidCommandException extends Exception {
-    public InvalidCommandException(String command){
-        System.out.println("The command " + command + " is invalid. Please double check.");
-    }
-}
