2fb15cacfc2eee8661ec8bbb4d72c51e25db4dca
Schwaida <60405166+Schwaida@users.noreply.github.com>
2022-06-11 16:05:22 +0300

    Add files via upload
    AWESOOOME

---
+++ src/Intro.java
@@ -0,0 +1,25 @@
+public abstract class Intro {
+    public static void OpeningText() throws InterruptedException{
+        System.out.println("Welcome, you new here?");
+        Thread.sleep(1050);
+        System.out.println("No problem you will get used to it");
+        Thread.sleep(1050);
+        System.out.println("You");
+        Thread.sleep(620);
+        System.out.println("are");
+        Thread.sleep(620);
+        System.out.println("about");
+        Thread.sleep(620);
+        System.out.println("to Play");
+        Thread.sleep(620);
+        System.out.println(" _______  _______  _        _        _______  _          _______  _______  ______   ______   _______  _______ \n" +
+                "(  ____ \\(  ___  )( (    /|( (    /|(  ___  )( (    /|  (  ____ \\(  ___  )(  __  \\ (  __  \\ (  ____ \\(  ____ )\n" +
+                "| (    \\/| (   ) ||  \\  ( ||  \\  ( || (   ) ||  \\  ( |  | (    \\/| (   ) || (  \\  )| (  \\  )| (    \\/| (    )|\n" +
+                "| |      | (___) ||   \\ | ||   \\ | || |   | ||   \\ | |  | (__    | |   | || |   ) || |   ) || (__    | (____)|\n" +
+                "| |      |  ___  || (\\ \\) || (\\ \\) || |   | || (\\ \\) |  |  __)   | |   | || |   | || |   | ||  __)   |     __)\n" +
+                "| |      | (   ) || | \\   || | \\   || |   | || | \\   |  | (      | |   | || |   ) || |   ) || (      | (\\ (   \n" +
+                "| (____/\\| )   ( || )  \\  || )  \\  || (___) || )  \\  |  | )      | (___) || (__/  )| (__/  )| (____/\\| ) \\ \\__\n" +
+                "(_______/|/     \\||/    )_)|/    )_)(_______)|/    )_)  |/       (_______)(______/ (______/ (_______/|/   \\__/\n" +
+                "                                                                                                              ");
+    }
+}
