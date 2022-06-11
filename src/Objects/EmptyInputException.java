2b7ecb8a638abda3e7664d1d2d3623bbf0894b86
Chainedge <chainedge@outlook.com>
2022-06-11 21:03:31 +0300

    Removed unimplemented exceptions, as thier functions are already built in

--- src/Objects/EmptyInputException.java
+++
@@ -1,8 +0,0 @@
-package Objects;
-
-public class EmptyInputException extends Exception {
-    public EmptyInputException(String input){
-        System.out.println(input + " is not acceptable.. Please enter your name without whitespaces.");
-    }
-
-}
--- src/Objects/InvalidSubjectException.java
+++
@@ -1,7 +0,0 @@
-package Objects;
-
-public class InvalidSubjectException extends Exception {
-    public InvalidSubjectException(String subject){
-        System.out.println("The subject " + subject + " is not valid.. Please double check.");
-    }
-}
